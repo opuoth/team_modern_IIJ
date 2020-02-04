5a09adb10a31a694dfe8122ad3416c308b0a5b8d
平山快 <hi1rayama@gmail.com>
2020-02-04 16:24:55 +0900

    中間発表のデモ

--- scripts/ping.js
+++ scripts/ping.js
@@ -23,34 +23,42 @@ module.exports = (robot) => {
     });
   });
 
-    /* 自転車を返す場所を探す処理と利用料金の目安を表示し、timerをstopする */
-    robot.respond(/F$/i, (res) => {
-      clearInterval(id);
-      res.send(`利用料金の目安:${i * 200}円\n利用時間:${i}時間`);
+  /* 自転車を返す場所を探す処理と利用料金の目安を表示し、timerをstopする */
+  robot.respond(/F$/i, (res) => {
+     /**タイマーのストップと利用料金の目安を表示 */
+     clearInterval(id);
+     res.send(`利用料金の目安:${i * 200}円\n利用時間:${i}時間`);
+    /**返す場所の候補を表示する */
+    res.send(`現在地周辺の自転車置き場を3つ表示します`);
+    res.send('自転車置き場1');
+    res.send('自転車置き場2');
+    res.send('自転車置き場3');
+    res.send({
+      question: 'どの自転車置き場を選択しますか?',
+      options: ['自転車置き場1', '自転車置き場2', '自転車置き場3'],
     });
 
+   
+  });
 
-  /* yesの場合は、周辺の自転車を探す処理　Noの場合は、ユーザからの返答を待つ */
+
+  /* yesの場合は、周辺の自転車を探す処理.Noの場合は、ユーザからの返答を待つ */
   robot.respond('yesno', (res) => {
     if (res.json.response === true) {
-      res.send(`周辺の自転車を探す処理：　`);
-      /*１時間毎に通知処理 */
+      /** 借りる候補の自転車ステーションを表示する処理*/
+      res.send('自転車置き場1\n安い料金');
+      res.send('自転車置き場2\n安い料金');
+      res.send('自転車置き場3\n安い料金');
       res.send({
-        text: '返す時はFを押してね',
-        onsend: () => {
-          let fn = () => {
-            res.send(`${i}時間経過しました`);
-            res.send(`現在の料金目安は${i * 200}円`);
-            i++;
-          }
-          id = setInterval(fn, tm);
-
-        }
+        question: 'どの自転車置き場を選択しますか?',
+        options: ['1を借りる', '2を借りる', '3を借りる']
       });
     } else {
       res.send(`探したくなったら「Hey」って言ってね`);
     }
   });
+
+
 
 
   /* 再度自転車の場所を探したくなったらまた表示する */
@@ -61,5 +69,56 @@ module.exports = (robot) => {
   });
 
 
+
+  /**自転車置き場の場所をユーザに選んでもらい、それを表示する */
+  robot.respond('select', (res) => {
+
+    if (res.json.options[res.json.response] === '1を借りる') {
+      res.send(`自転車1を借ました`);
+      /*１時間毎に通知処理 */
+      res.send({
+        text: '返す時はFを押してね',
+        onsend: () => {
+          let fn = () => {
+            res.send(`${i}時間経過しました`);
+            res.send(`現在の料金目安は${i * 200}円`);
+            i++;
+          }
+          id = setInterval(fn, tm);
+        }
+      });
+      onsend: (sent) => {
+        close_select: `completed. messageId: ${sent.message.id}`;
+      }
+    } else if (res.json.options[res.json.response] === '2を借りる') {
+      res.send(`自転車2を借ました`);
+      /*１時間毎に通知処理 */
+      res.send({
+        text: '返す時はFを押してね',
+        onsend: () => {
+          let fn = () => {
+            res.send(`${i}時間経過しました`);
+            res.send(`現在の料金目安は${i * 200}円`);
+            i++;
+          }
+          id = setInterval(fn, tm);
+        }
+      });
+    } else if (res.json.options[res.json.response] === '3を借りる') {
+      res.send(`自転車3を借ました`);
+      /*１時間毎に通知処理 */
+      res.send({
+        text: '返す時はFを押してね',
+        onsend: () => {
+          let fn = () => {
+            res.send(`${i}時間経過しました`);
+            res.send(`現在の料金目安は${i * 200}円`);
+            i++;
+          }
+          id = setInterval(fn, tm);
+        }
+      });
+    }
+  });
 
 }
